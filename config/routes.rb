3d12f43f9233b5eda2c7dff3337e87243714af22
n-ikeda-being <n-ikeda@beingcorp.co.jp>
2022-10-27 10:06:17 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  get 'follows/followings'
+  get 'follows/followers'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
   resources :books
   resources :users, only: %i(index show)
+  resources :users do
+    resource :follows
+    get 'followings' => 'follows#followings'
+    get 'followers' => 'follows#followers'
+  end
 end
