133e3481802c78624d2b88ac83743a2d7a376d9a
n-ikeda-being <n-ikeda@beingcorp.co.jp>
2022-10-31 11:30:59 +0900

    ログイン後の画面推移変更・名前以下の項目のfieldをemailからtextに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+    def after_sign_in_path_for(resource)
+        books_path # ログイン後に遷移する画面
+      end
+
+      def after_sign_out_path_for(resource)
+        new_user_session_path # ログアウト後に遷移する画面
+      end
 end
