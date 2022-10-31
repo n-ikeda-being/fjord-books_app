3797ea5112d9dbf889584f6faaa17be199d35dcd
n-ikeda-being <n-ikeda@beingcorp.co.jp>
2022-10-31 17:09:49 +0900

    投稿者の情報を取得する実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_31_071011) do
+ActiveRecord::Schema.define(version: 2022_10_31_080031) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -63,6 +63,8 @@ ActiveRecord::Schema.define(version: 2022_10_31_071011) do
     t.text "memo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "user_id"
+    t.string "user_name"
   end
 
   create_table "users", force: :cascade do |t|
