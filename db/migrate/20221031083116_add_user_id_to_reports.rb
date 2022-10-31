21042a527069b1c271d6516a552f8f6480141fd3
n-ikeda-being <n-ikeda@beingcorp.co.jp>
2022-10-31 17:36:34 +0900

    投稿者のみ編集・削除ができるよう修正

--- db/migrate/20221031083116_add_user_id_to_reports.rb
+++ db/migrate/20221031083116_add_user_id_to_reports.rb
@@ -1,6 +1,6 @@
 class AddUserIdToReports < ActiveRecord::Migration[6.1]
   def change
-    add_column :reports, :user_id, :string
+    add_column :reports, :user_id, :integer
     add_column :reports, :user_name, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_31_080031) do
+ActiveRecord::Schema.define(version: 2022_10_31_083116) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 2022_10_31_080031) do
     t.text "memo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "user_id"
+    t.integer "user_id"
     t.string "user_name"
   end
 
