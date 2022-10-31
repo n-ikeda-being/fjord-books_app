f7d2493a6620965406f7e398a2ab26b23efa4ba5
n-ikeda-being <n-ikeda@beingcorp.co.jp>
2022-10-31 13:21:06 +0900

    追加したカラムのDB保存、日本語化追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user! # ユーザがログインしていない場合はログインページにリダイレクト
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :postcode, :address, :profile])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :postcode, :address, :profile])
+    end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
+
     def after_sign_in_path_for(resource)
         books_path # ログイン後に遷移する画面
       end
