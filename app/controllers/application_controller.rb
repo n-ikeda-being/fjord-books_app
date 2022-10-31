8950f6fc6d93332b81707add57736cfb5fb43460
n-ikeda-being <n-ikeda@beingcorp.co.jp>
2022-10-31 13:46:39 +0900

    DBに保存するカラムをまとめる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,19 +5,21 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :postcode, :address, :profile])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :postcode, :address, :profile])
-    end
-
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+      keys = %i[name postcode address profile]
+      devise_parameter_sanitizer.permit(:sign_up, keys: keys)
+      devise_parameter_sanitizer.permit(:account_update, keys: keys)
     end
 
     def after_sign_in_path_for(resource)
         books_path # ログイン後に遷移する画面
       end
 
-      def after_sign_out_path_for(resource)
+    def after_sign_out_path_for(resource)
         new_user_session_path # ログアウト後に遷移する画面
-      end
+    end
+
+    def signed_in_root_path(_resource_or_scope)
+      books_path # アカウント編集後に遷移する画面
+    end
+
 end
