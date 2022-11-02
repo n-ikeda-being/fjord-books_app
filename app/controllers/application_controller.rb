fd907b383fbb51559a92b0cfae42c9aa5f715c6f
n-ikeda-being <n-ikeda@beingcorp.co.jp>
2022-11-02 16:30:44 +0900

    アカウント編集後の処理を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,23 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user! # ユーザがログインしていない場合はログインページにリダイレクト
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    def configure_permitted_parameters
-      keys = %i[name postcode address profile]
-      devise_parameter_sanitizer.permit(:sign_up, keys: keys)
-      devise_parameter_sanitizer.permit(:account_update, keys: keys)
-    end
+  def configure_permitted_parameters
+    keys = %i[name postcode address profile]
+    devise_parameter_sanitizer.permit(:sign_up, keys: keys)
+    devise_parameter_sanitizer.permit(:account_update, keys: keys)
+  end
 
-    def after_sign_in_path_for(resource)
-        books_path # ログイン後に遷移する画面
-      end
+  # ログイン後
+  def after_sign_in_path_for(resource_or_scope)
+      books_path
+  end
 
-    def after_sign_out_path_for(resource)
-        new_user_session_path # ログアウト後に遷移する画面
-    end
-
-    def signed_in_root_path(resource)
-      users_path # アカウント編集後に遷移する画面
-    end
+  # ログアウト後
+  def after_sign_out_path_for(resource_or_scope)
+      new_user_session_path
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  devise_for :users
+  devise_for :users, controllers: {registrations: 'users/registrations'}
   resources :books
   resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
