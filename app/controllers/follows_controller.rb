465e454d69203000d0dea59159f4fcf3b735391f
n-ikeda-being <n-ikeda@beingcorp.co.jp>
2022-11-16 17:28:14 +0900

    Railsガイドを参考に、フォロー/アンフォローの実装を修正

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -3,22 +3,16 @@
 class FollowsController < ApplicationController
   # フォローする
   def create
-    user = params[:user_id]
-    if current_user.follow(user)
-      redirect_to user_path(user), notice: 'フォローしました'
-    else
-      redirect_to  user_path(user), alert: 'フォローできませんでした（既にフォローしている可能性があります）'
-    end
+    user = User.find(params[:followed_id])
+    current_user.follow(user)
+    redirect_to user
   end
 
   # アンフォロー
   def destroy
-    user = params[:user_id]
-    if current_user.unfollow(user)
-      redirect_to user_path(user), notice: 'フォローを解除しました'
-    else
-      redirect_to  user_path(user), alert: 'フォローを解除できませんでした'
-    end
+    user = Relationship.find(params[:id]).followed
+    current_user.unfollow(user)
+    redirect_to user
   end
 
 end
